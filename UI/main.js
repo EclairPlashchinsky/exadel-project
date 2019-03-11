22b16653ef3260864ce1445991a655a8b622b83c
EclairPlashchinsky <ecler_pla@tut.by>
2019-03-11 11:11:53 +0300

    added java script file

---
+++ UI/main.js
@@ -0,0 +1,357 @@
+
+(function() {
+    var photoPosts = [   
+{
+  id: '1',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['SoMe'],
+  likes: ['mother', 'sonnofmomsfriend', 'dog', 'AJR']
+ },
+ {
+  id: '2',
+  descriprion: 'Look at me touching stuff!',
+  createdAt: new Date('2018-03-11T23:16:22'),
+  author: 'user_73304',
+  photoLink: 'picture2.jpg',
+  hashTags: ['SoMe', 'Relatable'],
+  likes: ['mother', 'sonnofmomsfriend',]
+ },
+ {
+  id: '3',
+  descriprion: 'My friends are the best!',
+  createdAt: new Date('2018-03-09T23:23:22'),
+  author: 'user_73304',
+  photoLink: 'picture3.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '4',
+  descriprion: 'Coming back to university...',
+  createdAt: new Date('2018-03-09T23:8:02'),
+  author: 'user_73304',
+  photoLink: 'picture4.jpg',
+  hashTags: ['bored'],
+  likes: ['sonnofmomsfriend']
+ },
+ {
+  id: '5',
+  descriprion: 'So beautiful!',
+  createdAt: new Date('2018-03-09T23:5:02'),
+  author: 'Mr. Snow',
+  photoLink: 'picture5.jpg',
+  hashTags: ['wow'],
+  likes: ['mother']
+ },
+ {
+  id: '6',
+  descriprion: 'When you need to make a website in a week',
+  createdAt: new Date('2018-03-01T19:41:12'),
+  author: 'user_73304',
+  photoLink: 'picture6.jpg',
+  hashTags: ['amiright'],
+  likes: ['mother', 'AJR', 'sonnofmomsfriend']
+ },
+ {
+  id: '7',
+  descriprion: 'Look what I found in my grandma house!',
+  createdAt: new Date('2018-03-01T18:55:12'),
+  author: 'Mr. Snow',
+  photoLink: 'picture7.jpg',
+  hashTags: ['old'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '8',
+  descriprion: 'Did u miss me?',
+  createdAt: new Date('2018-03-01T17:35:12'),
+  author: 'user_73304',
+  photoLink: 'picture8.jpg',
+  hashTags: ['Me', 'hashtad'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '9',
+  descriprion: 'Good morning!',
+  createdAt: new Date('2018-03-01T09:55:12'),
+  author: 'Mr. Snow',
+  photoLink: 'picture9.jpg',
+  hashTags: ['early'],
+  likes: ['mother', 'dad', 'AJR']
+ },
+ {
+  id: '10',
+  descriprion: 'Hitting depression once again!',
+  createdAt: new Date('2018-03-01T23:04:20'),
+  author: 'user_73304',
+  photoLink: 'picture10.jpg',
+  hashTags: ['Sameoldme', 'hashtag'],
+  likes: ['depression']
+ },
+ {
+  id: '11',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '12',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '13',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '14',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '15',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '16',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '17',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '18',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '19',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ },
+ {
+  id: '20',
+  descriprion: 'Me, when I need to work on the cite',
+  createdAt: new Date('2018-03-10T23:19:12'),
+  author: 'user_73304',
+  photoLink: 'picture1.jpg',
+  hashTags: ['BFF', 'Evening'],
+  likes: ['mother', 'dad', 'sonnofmomsfriend']
+ }
+];
+    
+    var heading = document.querySelector('changingcolor');
+      setInterval(function () {
+        var hue = 'rgb(' + (Math.floor(Math.random() * 256)) +
+          ',' +
+          (Math.floor(Math.random() * 256)) +
+          ',' +
+          (Math.floor(Math.random() * 256)) + ')';
+       heading.style.color = hue;
+      }, 1000);
+
+    function sort (){
+        var temp;
+        var id;
+        for(var j = 1; j < photoPosts.length-1; j++){
+            for(var i = j; i > 0; i--){
+                if(photoPosts[i-1].createdAt.getTime() < photoPosts[i].createdAt.getTime()){
+                    temp = photoPosts[i];
+                    photoPosts[i] = photoPosts[i-1], 
+                    photoPosts[i-1] = temp;
+                }
+            }
+        }
+    }
+
+    function getPhotoPosts(skip, top, filterConfig) {
+        sort();
+        if (skip === undefined) {
+          skip = 0;
+        }
+        if (top == undefined) {
+          top = 10;
+        }
+        temp = [];
+        for (var i = skip; top!=0 && i < photoPosts.length; i++) {
+          
+                if(filterConfig == undefined || 
+                    isappropriate(photoPosts[i], filterConfig)){
+                    if(addPhotoPost(temp, photoPosts[i])){
+                        top--;
+                    
+                }
+            }
+        }
+        return temp;
+      }
+
+    function isappropriate(photoPost, filterConfig){
+        if(filterConfig.author !== undefined){
+            if(filterConfig.author == photoPost.author){
+                return true;
+            }
+            else return false;
+        }
+        if(filterConfig.createdAt !== undefined){
+            if(filterConfig.createdAt.getDate() == photoPost.createdAt.getDate()
+            && filterConfig.createdAt.getFullYear() == photoPost.createdAt.getFullYear()
+            && filterConfig.createdAt.getMonth() == photoPost.createdAt.getMonth()
+            ){
+                return true;
+            }
+            else return false;
+        }
+        if(filterConfig.hashTags !== undefined){
+            if(filterConfig.hashTags == photoPost.hashTags){
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+    
+    function addPhotoPost(mas, photoPost){
+        if(validatePhotoPost(photoPost)){
+            mas[mas.length] = photoPost;
+            return true;
+        }
+        else return false;
+    }
+
+    function validatePhotoPost(photoPost){
+        if(typeof(photoPost.id) == typeof('1') &&
+        typeof(photoPost.hashTags) == typeof(['1']) &&
+        typeof(photoPost.descriprion) == typeof('1') &&
+        typeof(photoPost.author) == typeof('1') &&
+        typeof(photoPost.createdAt) == typeof(new Date) &&
+        typeof(photoPost.photoLink) == typeof('1'))
+            return true;
+        else return false;
+    }
+
+    function getPhotoPost(id){
+        for(var i = 0; i < photoPosts.length; i++){
+            if(photoPosts[i].id == id){
+                return photoPosts[i];
+            }
+        }
+        var temp = photoPosts[0];
+        temp.descriprion = 'NO SUCH ID';
+        temp.id = '0';
+        temp.photoLink = null;
+        temp.hashTags = null;
+        temp.author = null;
+        return temp;
+    }   
+
+    function editPhotoPost(id, photoPost){
+        for (var i = 0; i < photoPosts.length; i++) {
+            if (photoPosts[i].id == id){
+                if(photoPost.descriprion !== undefined){
+                    photoPosts[i].descriprion = photoPost.descriprion;
+                }
+                if(photoPost.hashTags !== undefined){
+                        photoPosts[i].hashTags = photoPost.hashTags;
+                    
+                }
+                if(photoPost.photoLink !== undefined){
+                    photoPosts[i].photoLink = photoPost.photoLink;
+                }
+            return true;
+            }
+        }
+        return false;
+    }
+
+    function removePhotoPost(id) {
+        for (var i = 0; i < photoPosts.length; i++) {
+          if (photoPosts[i].id == id) {
+            photoPosts.splice(i, 1);
+            return true;
+          }
+        }
+        return false;
+      }
+    photoPost = {
+        author: 'Mr. Snow',
+        hashTags: 'newhashtag'
+    };
+
+    console.log('first ellement"s time of creation before sort:');
+    console.log(photoPosts[0].createdAt);
+    console.log(photoPosts[1].createdAt);
+    sort();
+    console.log('After:');
+    console.log(photoPosts[0].createdAt);
+    console.log(photoPosts[1].createdAt);
+
+    console.log('photos of Mr. Snow:');
+    var example = getPhotoPosts(0, 5, photoPost);
+    
+    for( var i = 0; i < example.length; i++){
+    console.log(example[i]);
+    }
+
+    photoPost.author = 'Dad';
+    console.log('photos of Dad:');
+    var example = getPhotoPosts(0, 5, photoPost);
+    for( var i = 0; i < example.length; i++){
+        console.log(example[i]);
+        }
+
+    editPhotoPost(5, photoPost);
+    console.log('Photo with id 5 with changed hashtag:')
+    console.log(getPhotoPost(5));
+
+    photo = {
+        id: 1
+    }
+
+    console.log('Search with wrong id:');
+    console.log(getPhotoPost(photo));
+}()); 
+
